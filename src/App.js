2d09fb5d05a2c6d6b202f57f476969a307882a19
Ju-ae <qowndo@gmail.com>
2022-01-15 14:45:37 +0900

    Add firebase to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <body>
         <h3>hello!</h3>
-      </body>
     </div>
   );
 }
